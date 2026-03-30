c10b6787eadcb2e33584834dbade9953f0117c98
Amirullo <ibaydillayevamirullo@gmail.com>
2026-03-31 00:42:15 +0500

    build234

--- frontend/src/pages/dashboard/assetCategories/assetCategoriesModal/AssetCategoriesModal.jsx
+++ frontend/src/pages/dashboard/assetCategories/assetCategoriesModal/AssetCategoriesModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./assetCategoriesModal.module.css";
+import styles from "./assetcategoriesModal.module.css";
 import axiosInstance from "../../../../api/axiosInstanse";
 import { toast } from "react-toastify";
 
